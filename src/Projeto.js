9566907c5be1dbbb6074e16f75b14593a4de51da
Yohran Kallahari <yohranv@gmail.com>
2023-07-26 22:41:41 -0300

    Finalizado página de login/register | e alterações no projeto principal

--- src/Projeto.js
+++ src/Projeto.js
@@ -32,88 +32,81 @@ export default function Projeto(props){
             </div>
             {/* ================================== NAV ========================================== */}
             <div className="contents">
-                <div className="conteudo">
-                    {/* <div className="c1"></div> */}
-                    <img className="bolo_capa" src={bolo_capa} alt="Bolo" />
-                </div>
-                
-                <div className="conteudo">
+                    <div className="conteudo">
+                        <img className="bolo_capa" src={bolo_capa} alt="Bolo" />
+                    </div>
                     
-                    <div className="arm">
-                        <div className="c2">
-                            <h2 className="h2">MEUS <span>BOLOS</span> SÃO <div>CONFECCIONADOS</div> COM AMOR.</h2>
-                            <div className="textC2">
-                                <p>Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em “Editar Texto” ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sou um ótimo espaço para você contar sua história.</p>
-                            </div>                        
+                    <div className="conteudo">
+                        
+                        <div className="arm">
+                            <div className="c2">
+                                <h2 className="h2">MEUS <span>BOLOS</span> SÃO <div>CONFECCIONADOS</div> COM AMOR.</h2>
+                                <div className="textC2">
+                                    <p>Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em “Editar Texto” ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sou um ótimo espaço para você contar sua história.</p>
+                                </div>                        
+                            </div>
+                        </div>
+
+                        <div className="arm">
+                            <img className="bolo_c3" src={bolo_c3} alt="Bolo_c3"></img>
+                        </div>
+
+                    </div>
+
+                    <div className="conteudo">
+                        <div className="arm c4"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Faça seu pedido online</div></a> </div>
+
+                        <div className="arm c5"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Conheça o confeiteiro</div></a> </div>
+
+                        <div className="arm c6"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Personalize o seu bolo</div></a> </div>
+                    </div>
+
+                {/* </div> */}
+                {/* ============================================================================================= */}
+                <p>MEU ACERVO DE BOLOS</p>
+                <br></br>
+                <div className="slides">
+                    <div className="elements">
+
+                        <input type="radio" name="radio-btn" id="radio1" />
+                        <input type="radio" name="radio-btn" id="radio2" />
+                        <input type="radio" name="radio-btn" id="radio3" />
+                        <input type="radio" name="radio-btn" id="radio4" />
+                        <input type="radio" name="radio-btn" id="radio5" />
+                        <input type="radio" name="radio-btn" id="radio6" />
+
+                        <div className="slide first">
+                            <img src={bolo_slide1} alt="Bolo_slide"></img>
+                        </div>
+                        <div className="slide">
+                            <img src={bolo_slide2} alt="Bolo_slide"></img>
+                        </div>
+                        <div className="slide">
+                            <img src={bolo_slide3} alt="Bolo_slide"></img>
+                        </div>
+                        <div className="slide">
+                            <img src={bolo_slide4} alt="Bolo_slide"></img>
+                        </div>
+                        <div className="slide">
+                            <img src={bolo_slide5} alt="Bolo_slide"></img>
+                        </div>
+                        <div className="slide">
+                            <img src={bolo_slide6} alt="Bolo_slide"></img>
                         </div>
                     </div>
 
-                    <div className="arm">
-                        <img className="bolo_c3" src={bolo_c3} alt="Bolo_c3"></img>
-                    </div>
-
-                </div>
-
-                <div className="conteudo">
-                    <div className="arm c4"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Faça seu pedido online</div></a> </div>
-
-                    <div className="arm c5"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Conheça o confeiteiro</div></a> </div>
-
-                    <div className="arm c6"> <a className=" box-pedido" href="https://jsonplaceholder.typicode.com/todos"><div className="pedido">Personalize o seu bolo</div></a> </div>
+                        <div className="navigation">
+                            <label htmlFor="radio1" className="barra"></label>
+                            <label htmlFor="radio2" className="barra"></label>
+                            <label htmlFor="radio3" className="barra"></label>
+                            <label htmlFor="radio4" className="barra"></label>
+                            <label htmlFor="radio5" className="barra"></label>
+                            <label htmlFor="radio6" className="barra"></label>
+                        </div>   
                 </div>
 
             </div>
-            <hr></hr>
-            <br></br>
             {/* ============================================================================================= */}
-            <p>MEU ACERVO DE BOLOS</p>
-            <br></br>
-            <div className="slides">
-                <div className="elements">
-
-                    <input type="radio" name="radio-btn" id="radio1" />
-                    <input type="radio" name="radio-btn" id="radio2" />
-                    <input type="radio" name="radio-btn" id="radio3" />
-                    <input type="radio" name="radio-btn" id="radio4" />
-                    <input type="radio" name="radio-btn" id="radio5" />
-                    <input type="radio" name="radio-btn" id="radio6" />
-
-                    <div className="slide first">
-                        <img src={bolo_slide1} alt="Bolo_slide"></img>
-                    </div>
-                    <div className="slide">
-                        <img src={bolo_slide2} alt="Bolo_slide"></img>
-                    </div>
-                    <div className="slide">
-                        <img src={bolo_slide3} alt="Bolo_slide"></img>
-                    </div>
-                    <div className="slide">
-                        <img src={bolo_slide4} alt="Bolo_slide"></img>
-                    </div>
-                    <div className="slide">
-                        <img src={bolo_slide5} alt="Bolo_slide"></img>
-                    </div>
-                    <div className="slide">
-                        <img src={bolo_slide6} alt="Bolo_slide"></img>
-                    </div>
-                </div>
-
-                    <div className="navigation">
-                        <label htmlFor="radio1" className="barra"></label>
-                        <label htmlFor="radio2" className="barra"></label>
-                        <label htmlFor="radio3" className="barra"></label>
-                        <label htmlFor="radio4" className="barra"></label>
-                        <label htmlFor="radio5" className="barra"></label>
-                        <label htmlFor="radio6" className="barra"></label>
-                    </div>
-
-                
-            </div>
-
-            {/* ============================================================================================= */}
-            <br></br>
-            <hr></hr>
-            <br></br>
             <div className="footerOrigin">
                 <div className="boxs">
                     <h3>Inicio</h3>
@@ -142,16 +135,17 @@ export default function Projeto(props){
 
                 <div className="boxs">
                     <h3>Sobre Nós</h3>
-                    <p>Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum 
-                        lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum 
-                        Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem 
-                        ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem  
-                        ipsum lorem ipsum Lorem ipsum lorem ipsum</p>
+                    <p>
+                        A Tradicional Bolos e Tortas atende múltiplos paladares e dietas alimentares, com uma variedade exclusiva de produtos elaborados especialmente para você. Dos Bolos artesanais, aos Bolos Funcionais, Zero Açúcar, Vegano, Tortas Salgadas, porções individuais e Sobremesas cheias de sabor. Nós cuidamos de cada detalhe e preparamos tudo artesanalmente para que você sinta felicidade em cada pedaço.
+                        <br></br><br></br>
+                        Afinal, Tradicional é fazer parte da sua vida!
+                    </p>
                 </div>
             </div>
             
             <div className="footer">
                 <h2 className="iconn"><div className="iconFooter"></div></h2>
+                <div className="author">Yohran Kallahari | Sr-Mackfly &copy;</div>
                 <div className="socials">
                     <div className="social">
                         <a href="https://www.facebook.com/yohran.venancio/"><div className="face"></div></a>
@@ -165,7 +159,8 @@ export default function Projeto(props){
                 </div>
             </div>
 
-            <script src="./carrocel.js"></script>
-        </div>       
+            
+        </div>      
+         
     )
 }
