b46f0224ea814b726af998f40d55f7f6072369f4
Yohran Kallahari <yohranv@gmail.com>
2023-08-07 22:35:24 -0300

    Criando um cabeçalho na página de pedidos

--- src/components/PedidoOn.js
+++ src/components/PedidoOn.js
@@ -8,7 +8,24 @@ export default function PedidoOn(props){
         <div>
             <Header></Header>
             <div className="containerPedidos">
-                Pedidos Online (EM DESENVOLVIMENTO)
+                <div className="pedidosBox">
+                    <div className="headerPedidos">
+                        <div className="retirada"><span>Retirada, o mais rápido possível (em 15 minutos)</span><div className="alterar">Alterar</div></div>
+                        <div className="HeaderMenuPedidos">
+                            <div className="headerMenu">
+                                <div className="menu-bolo"><p>MENU DE BOLOS</p></div> 
+                                <div className="headerP"><p>Bolos de Casamento</p></div> 
+                                <div className="headerP"><p>Personalize seu bolo</p></div>
+                            </div>
+                        </div>
+                        <div className="headerInfo">
+                            <h2>MENU DE BOLOS</h2>
+                            <p>Encomendar com 3 dias de antecedência</p>
+                        </div>
+                    
+                    </div>
+
+                </div>
                 
             </div>
             
